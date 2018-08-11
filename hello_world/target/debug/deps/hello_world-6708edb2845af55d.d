669f3c9f21bee377cf33489b21d51a85ce80990f
banrutech <banrutech@gmail.com>
2018-08-11 17:26:59 +0900

    first commit

---
+++ hello_world/target/debug/deps/hello_world-6708edb2845af55d.d
@@ -0,0 +1,5 @@
+C:\Users\bannru\Documents\work\rust\hello_world\target\debug\deps\hello_world-6708edb2845af55d.exe: src\main.rs
+
+C:\Users\bannru\Documents\work\rust\hello_world\target\debug\deps\hello_world-6708edb2845af55d.d: src\main.rs
+
+src\main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+C:\Users\bannru\Documents\work\rust\hello_world\target\debug\hello_world.pdb: C:\Users\bannru\Documents\work\rust\hello_world\src\main.rs
